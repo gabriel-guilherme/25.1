3b6c82182bd1033b7182ed33d26e7878b0e78ecd
Gabriel Guilherme <gabriel.guilherme.131@ufrn.edu.br>
2025-06-02 18:18:19 -0300

    soco ajustado

--- Player/player.gd
+++ Player/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 @export var movement_speed = 40.0
 @export var hp = 80
+var maxHp = 80
+var attack_cooldown = 0
+var attack_size = 0
 @onready var sprite = $AnimatedSprite2D
 
 var experience = 0
@@ -29,10 +32,10 @@ var banana_baseammo = 1
 @export var banana_level = 1
 
 # Punch
-var punch_ammo = 0
-var punch_baseammo = 1
-@export var punch_attackspeed = 1.5
-@export var punch_level = 0
+var punch_ammo = 1
+var punch_baseammo = 0
+@export var punch_attackspeed = 0.5
+@export var punch_level = 1
 
 # Banana Peel
 @export var banana_peel_attackspeed = 4.0
@@ -53,6 +56,13 @@ var enemy_close_punch = []
 @onready var upgradeOptions = get_node("%UpgradeOptions")
 @onready var itemOptions = preload("res://utility/item_option.tscn")
 @onready var soundLevelUp = get_node("%sound_levelup")
+
+#UPGRADES
+var collected_upgrades = []
+var upgrade_options = []
+var armor = 0
+var speed = 0
+var spell_size = 0
 
 func _ready():
 	attack()
@@ -84,17 +94,17 @@ func movement():
 
 func attack():
 	if banana_level > 0:
-		bananaTimer.wait_time = banana_attackspeed
+		bananaTimer.wait_time = banana_attackspeed * (1-attack_cooldown)
 		if bananaTimer.is_stopped():
 			bananaTimer.start()
 
 	if punch_level > 0:
-		punchTimer.wait_time = punch_attackspeed
+		punchTimer.wait_time = punch_attackspeed * (1-attack_cooldown)
 		if punchTimer.is_stopped():
 			punchTimer.start()
 
 	if banana_peel_level > 0:
-		bananaPeelTimer.wait_time = banana_peel_attackspeed
+		bananaPeelTimer.wait_time = banana_peel_attackspeed * (1-attack_cooldown)
 		if bananaPeelTimer.is_stopped():
 			bananaPeelTimer.start()
 
@@ -110,24 +120,24 @@ func _on_banana_timer_timeout():
 
 func _on_banana_attach_timer_timeout():
 	if banana_ammo > 0:
-		var banana_attack = banana.instantiate()
-		banana_attack.position = position
-		banana_attack.target = get_random_target_from(enemy_close)
-		banana_attack.level = banana_level
-		add_child(banana_attack)
-		banana_ammo -= 1
-
-		if banana_ammo > 0:
-			bananaAttackTimer.start()
-		else:
-			bananaAttackTimer.stop()
+		for i in range(banana_ammo):
+			var banana_attack = banana.instantiate()
+			banana_attack.position = position
+			banana_attack.target = get_random_target_from(enemy_close)
+			banana_attack.level = banana_level
+			add_child(banana_attack)
+		
+		banana_ammo = 0
+	bananaAttackTimer.stop()
 
 func _on_punch_timer_timeout():
+	print("PunchTimer timeout - adding ammo")
 	if enemy_close_punch.size() > 0:
 		punch_ammo += punch_baseammo
 		punchAttackTimer.start()
 
 func _on_punch_attach_timer_timeout():
+	print("PunchAttackTimer timeout - ammo:", punch_ammo)
 	if punch_ammo > 0 and enemy_close_punch.size() > 0:
 		var punch_attack = punch.instantiate()
 		punch_attack.position = position
@@ -184,7 +194,7 @@ func _on_enemy_detection_2_body_exited(body: Node2D):
 
 # ========== DAMAGE ==========
 func _on_hurt_box_hurt(damage: Variant, _angle, _knockback):
-	hp -= damage
+	hp -= clamp(damage - armor, 1.0, 999.0)
 	print(hp)
 
 
@@ -241,15 +251,114 @@ func levelup():
 	var optionsmax = 3
 	while options < optionsmax:
 		var option_choice = itemOptions.instantiate()
+		option_choice.item = get_random_item()
 		upgradeOptions.add_child(option_choice)
 		options += 1
 	get_tree().paused = true
 
 func upgrade_character(upgrade):
+	match upgrade:
+		# Banana throw
+		"banana1":
+			banana_level = 1
+			banana_baseammo += 1
+		"banana2":
+			banana_level = 2
+			banana_baseammo += 1
+		"banana3":
+			banana_level = 3
+			banana_baseammo += 1
+		"banana4":
+			banana_level = 4
+			banana_baseammo += 1
+		
+		# Punch
+		"punch1":
+			punch_level = 1
+			punch_baseammo += 1
+		"punch2":
+			punch_level = 2
+			punch_baseammo += 1
+		"punch3":
+			punch_level = 3
+			punch_baseammo += 1
+		"punch4":
+			punch_level = 4
+			punch_baseammo += 1
+
+		# Banana peel
+		"banana_peel1":
+			print("aBABAwd")
+			banana_peel_level = 1
+		"banana_peel2":
+			banana_peel_level = 2
+		"banana_peel3":
+			banana_peel_level = 3
+		"banana_peel4":
+			banana_peel_level = 4
+
+		# Banana orbit
+		"banana_orbit1":
+			banana_orbit_level = 1
+			has_spawned_orbit = false
+		"banana_orbit2":
+			banana_orbit_level = 2
+			has_spawned_orbit = false
+		"banana_orbit3":
+			banana_orbit_level = 3
+			has_spawned_orbit = false
+		"banana_orbit4":
+			banana_orbit_level = 4
+			has_spawned_orbit = false
+
+		# Outros upgrades
+		"medic_kit":
+			hp += 20
+			hp = clamp(hp, 0, maxHp)
+		"coffee1", "coffee2", "coffee3", "coffee4":
+			attack_cooldown += 0.05
+		"apple1", "apple2", "apple3", "apple4":
+			movement_speed += 0.20
+		"whey1", "whey2", "whey3", "whey4":
+			attack_size += 0.10
+		"fur1", "fur2", "fur3", "fur4":
+			armor += 1
+	
+	
+	
+	
 	var option_children = upgradeOptions.get_children()
 	for i in option_children:
 		i.queue_free()
+	upgrade_options.clear()
+	collected_upgrades.append(upgrade)
 	levelPanel.visible = false
 	levelPanel.position = Vector2(800, 50)
 	get_tree().paused = false
 	calculate_experience(0)
+	
+	attack()
+
+func get_random_item():
+	var dblist = []
+	for i in UpgradeDb.UPGRADES:
+		if i in collected_upgrades: #find already collected upgrades
+			pass
+		elif i in upgrade_options:
+			pass
+		elif UpgradeDb.UPGRADES[i]["type"] == "item":
+			pass
+		elif UpgradeDb.UPGRADES[i]["prerequesite"].size() > 0 :
+			for n in UpgradeDb.UPGRADES[i]["prerequesite"]:
+				if not n in collected_upgrades:
+					pass
+				else:
+					dblist.append(i)
+		else:
+			dblist.append(i)
+	if dblist.size() > 0:
+		var randomitem = dblist.pick_random()
+		upgrade_options.append(randomitem)
+		return randomitem
+	else:
+		return null
