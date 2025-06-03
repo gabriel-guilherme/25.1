0046d8c8485aef9c93595bcfff0b5c9b54cc3ff7
Gabriel Guilherme <gabriel.guilherme.131@ufrn.edu.br>
2025-06-03 00:30:48 -0300

    enemySpawn e health bar

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -9,30 +9,33 @@ var knockback = Vector2.ZERO
 var is_stunned = false
 
 signal enemy_died
+signal remove_from_array(object)
 
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var loot_base = get_tree().get_first_node_in_group("loot")
 @onready var sprite = $AnimatedSprite2D
 @onready var sound_hit = $sound_hit
-
-#enemy spawner
 @onready var enemySpawner = get_node("/root/World/EnemySpawner")
 
-signal remove_from_array(object)
-
 var exp_gem = preload("res://Item/ExperienceGem/experience_gem.tscn")
+var is_dead = false
 
+# Propriedades base (para escalonamento)
+var base_hp = hp
+var current_damage := 0  # Ainda não usado, mas pronto para aplicar
+var base_damage := 10  # Valor genérico, pode ser usado em ataques
+
+func _ready():
+	# Salva o HP base para uso futuro (caso esteja usando o valor exportado em múltiplos inimigos diferentes)
+	pass
 
 func _physics_process(_delta):
-	
 	knockback = knockback.move_toward(Vector2.ZERO, knockback_recovery)
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction*movement_speed
-	
+	velocity = direction * movement_speed
 	velocity += knockback
-	
 	move_and_slide()
-	
+
 	if direction.x < 0.1:
 		sprite.flip_h = true
 	elif direction.x > -0.1:
@@ -44,38 +47,38 @@ func slow(duration, power):
 		movement_speed = 0
 	else:
 		movement_speed -= power
-		
-		
-		
 	await get_tree().create_timer(duration).timeout
 	movement_speed = old_movespeed
 
 func death():
+	if is_dead:
+		return
+	is_dead = true
 	enemySpawner.increase_defeated()
 	movement_speed = 0
 	sprite.play("disappear")
 	await sprite.animation_finished
-	
 	emit_signal("remove_from_array", self)
 	emit_signal("enemy_died")
-	
-	var new_gem = exp_gem.instantiate()
-	new_gem.global_position = global_position
-	new_gem.experience = experience
-	loot_base.call_deferred("add_child", new_gem)
 	queue_free()
 
-var is_dead = false  # Nova flag
-
-func _on_hurt_box_hurt(damage: Variant, angle, knockback_amount) -> void:
+func _on_hurt_box_hurt(damage: int, angle: Vector2, knockback_amount: float) -> void:
 	if is_dead:
-		return  # Se já estiver morrendo, ignora o resto
-
+		return
+		
+	print(hp, damage, "MAMA")
 	hp -= damage
 	knockback = angle * knockback_amount
 
 	if hp <= 0:
-		is_dead = true  # Marca como morto para evitar chamadas múltiplas
 		death()
 	else:
 		sound_hit.play()
+
+# 🆕 Recebe multiplicadores do WaveManager
+func apply_modifiers(h_mul: float, s_mul: float, d_mul: float):
+	hp = int(base_hp * h_mul)
+	movement_speed = base_movement_speed * s_mul
+	current_damage = base_damage * d_mul
+
+	print("Inimigo modificado - HP: %d | Vel: %.2f | Dano: %.2f" % [hp, movement_speed, current_damage])
